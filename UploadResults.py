e97bb798b370d0d39aa62c879255dcedb96561b2
Bhanu Prakash <bhanutechno@gmail.com>
2023-01-16 22:21:23 +0530

    Added Validations

--- UploadResults.py
+++ UploadResults.py
@@ -16,7 +16,8 @@ from lxml import etree
 import shutil
 
 class UploadResults():
-
+    
+    # Routine 1 : Runs the HL analysis using highlight automation jar file
     def runHLAnalysis(self,*args):
         process = Popen(['java', '-jar']+list(args), stdout=PIPE, stderr=PIPE)
         ret = []
@@ -31,15 +32,17 @@ class UploadResults():
             ret += str(stderr).split('\n')
         #ret.remove(b'')
         return ret
-
+    
+    #WIP - Will be used for validating if scan is completed successfully
     def checkScan(self,appl_id):
         url = f'domains/{self._hl_instance}/applications/{appl_id}/results'
         (status, json) = self.get(url)
         if status == codes.ok and len(json) > 0:
             return json
         else:
-            raise KeyError (f'No applications not found')
+            raise KeyError (f'Applications not found')
     
+    #WIP - Will be used for validating if scan is SCA is ready
     def checkSCA(self,appl_id):
         url = f'domains/{self._hl_instance}/applications/{appl_id}/results'
         (status, json) = self.get(url)
@@ -47,7 +50,8 @@ class UploadResults():
             return json
         else:
             raise KeyError (f'No applications found')
-
+    
+    #Routine 2 - Used to initiate the BOM request using the highlight instance
     def generateBOMRequest(self,appID,compID,basicAuth,cycloneDXPath):
         time.sleep(120)
         data={"selector": {"applications": [appID]}, "reportConfig": ["SendMail","DependenciesAndCve"]}
@@ -63,6 +67,7 @@ class UploadResults():
         if response.status_code==202:
             self.generateBOM(responseHeader,basicAuth,cycloneDXPath)
 
+    #Routine 3 - Used to generate the BOM in CycloneDX format (response.xml)
     def generateBOM(self,respHead,basicAuth,cycloneDXPath):
         iStatus=404
         iRetry=0
@@ -78,13 +83,18 @@ class UploadResults():
             with open(cycloneDXPath, "w+") as f:
                 f.write(response.text)
     
+    #Routine 4 - XML Parser used to parse cyclonedx output and extract the dependecnies from these and generate a new POM.XML file
+    # Note : This parser only works for generating pom.xml
     def xmlParsing(self,cycloneDXOutput,save_path_file,outputPOM,newOutputFolder):
         #mytree = ET.parse('C:\\DATA\\GITRepo\\com.castsoftware.uc.hl.dt\\response.xml',parser = ET.XMLParser(encoding = 'iso-8859-5'))
+        
+        # Start creating new pom.xml file
         mytree = ET.parse(cycloneDXOutput,parser = ET.XMLParser(encoding = 'iso-8859-5'))
         myroot = mytree.getroot()
         
         root = minidom.Document()
         
+        # ADD elements and attributes in xml file
         project=root.createElement('project')
         root.appendChild(project)
         project.setAttribute('xmlns','http://maven.apache.org/POM/4.0.0')
@@ -99,6 +109,7 @@ class UploadResults():
         xml = root.createElement('dependencies') 
         project.appendChild(xml)
 
+        #Reads cyclonedx output extract element values and place those in right location
         for dep in myroot.iter('{http://cyclonedx.org/schema/bom/1.3}dependency'):
             components=dep.get('ref')
             print(dep.get('ref'))
@@ -106,41 +117,49 @@ class UploadResults():
             depsChild = root.createElement('dependency')
             xml.appendChild(depsChild)
             
+            #Extract group ID
             groupID = root.createElement('groupId')
             textgroupID=root.createTextNode(components.partition(':')[0])
             #textgroupID=root.createTextNode(components)
             depsChild.appendChild(groupID)
             groupID.appendChild(textgroupID)
             
-
+            #Extract Artifact ID i.e. the name of component
             artifactId=root.createElement('artifactId')
             textartifactId=root.createTextNode(components.partition(':')[2].partition('@')[0])
             #textartifactId=root.createTextNode(components)
             depsChild.appendChild(artifactId)
             artifactId.appendChild(textartifactId)
 
+            #Extract component's version
             version=root.createElement('version')
             textversion=root.createTextNode(components.partition(':')[2].partition('@')[2])
             #textversion=root.createTextNode(components)
             depsChild.appendChild(version)
             version.appendChild(textversion)
 
-
+        #Make XML out of the extracted information
         xml_str = root.toprettyxml(indent ="\t") 
         
+        #Create a new directory inside source directory to place newly generated pom.xml
         if not os.path.exists(newOutputFolder):
             # if the demo_folder directory is not present 
             # then create it.
             os.makedirs(newOutputFolder)
-        outPath=os.listdir(newOutputFolder)    
+        outPath=os.listdir(newOutputFolder)
+
+        #Copy the pom.xml file in the new directory inside source location    
         if len(outPath)==0:
             shutil.copy(outputPOM,newOutputFolder)
         
+        #Read the file
         with open(save_path_file, "w+") as f:
             f.write(xml_str) 
         
+        #Remove duplicate elements from the file
         self.removeDuplicateTags(save_path_file,newOutputFolder)
-            
+
+    #Routine 5 - This routine will delete the duplicate elements from the newly generate xml        
     def removeDuplicateTags(self,save_path_file,outputPOM):
         
         outputPOM=outputPOM+'\pom.xml'
@@ -196,34 +215,36 @@ class UploadResults():
         global occurrences_latest
         occurrences_latest = data.count("<dependency>")   
 
+
+# -------------------START PROGRAM----------------------------------------------------------
 print('\nCAST HL Scan - Version 1.0')
 print('Copyright (c) 2022 CAST Software Inc.\n')
 print('If you need assistance, please contact Bhanu Prakash (BBA) from the CAST IN PS team\n')
 
  
 #HL Command line parameters
-#extract parameters from properties.txt file
+#Extract parameters from properties.json file
 dirname = os.path.dirname(__file__)
 properties_file=dirname+'\\Configuration\\properties.json'
 
 with open(properties_file, 'r') as f:
   data = json.load(f)
 
-hlJarPath=data["hlJarPath"]
-sourceDir=data["sourceDir"]
-workingDir=data["workingDir"]
-analyzerDir=data["analyzerDir"]
-companyId=data["companyId"]
-applicationId=data["applicationId"]
-snapshotLabel=data["snapshotLabel"]
-serverUrl=data["serverUrl"]
-basicAuth=data["basicAuth"]
-cycloneDXOutput=data["cyclodxOutput"]
-save_path_file=data["save_path_file"]
-outputPOM=data["outputpom"]
-newOutputFolder=data["newOutputFolder"]
+hlJarPath=data["hlJarPath"] #Location of highlight automation jar file
+sourceDir=data["sourceDir"] #Location of source directory
+workingDir=data["workingDir"] #Location of highlight working directory
+analyzerDir=data["analyzerDir"] #Location of highlight analyze directory
+companyId=data["companyId"] #Company ID
+applicationId=data["applicationId"] #Application ID
+snapshotLabel=data["snapshotLabel"] #Highlight Snapshot label
+serverUrl=data["serverUrl"] #Highlight instance URL
+basicAuth=data["basicAuth"] #Basic authentication combination of user_name:password
+cycloneDXOutput=data["cyclodxOutput"] #Location of generated cycloneDX file
+save_path_file=data["save_path_file"] #Location newly generated pom file
+outputPOM=data["outputpom"] #Intermediate Location of pom.xml file having duplicate tags removed
+newOutputFolder=data["newOutputFolder"] #New folder inside source code where newly generated pom.xml will be saved
 
-
+#Validation of parameters
 if hlJarPath=='' or str(hlJarPath).isspace():
     print('HL Aoutmation JAR path is not defined')
     exit()
@@ -233,7 +254,6 @@ if not os.path.exists(hlJarPath):
 if not hlJarPath.endswith('.jar'):
     print('HL Aoutmation JAR file does not exist')
     exit()
-
 
 if sourceDir=='' or str(sourceDir).isspace():
     print('Source Directory path is not defined')
@@ -352,13 +372,19 @@ if not os.path.exists(newOutputFolder):
 
 
 
-#Arguments to pass in HighlightAutomation 
+#Arguments to be passed in HighlightAutomation JAR (Routine 1) 
 args = [f'{hlJarPath}', '--sourceDir', f'{sourceDir}', '--workingDir' , f'{workingDir}', '--analyzerDir', f'{analyzerDir}', '--companyId', f'{companyId}', '--applicationId', f'{applicationId}', '--snapshotLabel', f'{snapshotLabel}', '--basicAuth', f'{basicAuth}', '--serverUrl', f'{serverUrl}'] # Any number of args to be passed to the jar file
 
 #Number of <dependency> tags pereviously and in latest POM XML
 occurrences_previous=0
 occurrences_latest=0
+
+#Instantiating main class 
 obj=UploadResults()
+
+#Iteration exit criteria
+# Criteria 1 - If Previous occurrences of tags are equal to latest occurrences
+# Criteria 2 - If occurrences reach 100
 for iter in range(100):
     if occurrences_previous!=occurrences_latest or occurrences_previous==0:    
         #1. Run HL Scan and upload results
@@ -383,12 +409,14 @@ for iter in range(100):
             print('Error occurred during Parsing BOM')
             exit()
     else:
+        #Remove intermediate cyclonedx output file
         if os.path.exists(cycloneDXOutput):
             for subdir, dirs, files in os.walk(cycloneDXOutput):
                 for file in files:
                     if file.endswith('response.xml'):
                         os.remove(file)
 
+        #Remove intermediate POM file
         if os.path.exists(save_path_file):
             for subdir, dirs, files in os.walk(cycloneDXOutput):
                 for file in files:
@@ -396,6 +424,3 @@ for iter in range(100):
                         os.remove(file)
 
         exit()
-
-
-   
